1d4b7994fa45738c6f08589bf33db42d67c58374
buidung <buidung314@gmail.com>
2021-08-31 10:59:21 +0700

    add ++

--- slider-animation.js
+++ slider-animation.js
@@ -1,19 +1,22 @@
 var slideIndex = 0;
+var slideImg = [
+  "/slider1.jpg",
+  "/slider2.jpg",
+  "/slider3.jpg",
+  "/slider4.jpg",
+  "/slider5.jpg"
+]
 var changeSlide = function() {
-  var slideImg = [
-    "/slider.jpg",
-    "/slider2.jpg",
-    "/slider3.jpg",
-    "/slider4.jpg",
-    "/slider5.jpg"
-  ]
+  
   document.querySelector(".slide-img").src =slideImg[slideIndex]
   slideIndex++
   if(slideIndex > slideImg.length-1) {
     slideIndex = 0
   }
   setTimeout(changeSlide, 4000)
-
+}
+function currentSile(n) {
+  changeSlide(slideIndex = n)
 }
 
 changeSlide()
